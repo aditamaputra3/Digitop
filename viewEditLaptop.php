c5d1e4a89a094a2b111f99a87fb50baa3cc74f4f
aditamaputra3 <aditamaputra3@gmail.com>
2023-04-04 14:04:30 +0700

    bismilah selesai

--- viewEditLaptop.php
+++ viewEditLaptop.php
@@ -17,57 +17,66 @@ $result = mysqli_query($koneksi, $query);
   <div class="container mt-4">
     <?php while ($row = mysqli_fetch_assoc($result)) : ?>
       <form action="actionEditLaptop.php" method="POST" class="row g-3">
-      <h1 class="mt-3">Edit Laptop</h1>
-        <div class="col">
-          <div class="form-floating mb-3">
-            <input name="id_laptop" class="form-control" value="<?php echo $row['id_laptop'] ?>" type="text" readonly />
-            <label for="id_laptop" class="form-label">ID Laptop</label>
+        <div class="card">
+          <div class="card-header">
+            <h2 class="mt-3">Edit Laptop</h2>
           </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <div class="form-floating mb-3">
+                  <input name="id_laptop" class="form-control" value="<?php echo $row['id_laptop'] ?>" type="text" readonly />
+                  <label for="id_laptop" class="form-label">ID Laptop</label>
+                </div>
 
-          <div class="form-floating mb-3">
-            <input name="tipe" class="form-control" value="<?php echo $row['tipe'] ?>" type="text" />
-            <label for="tipe" class="form-label">Tipe</label>
-          </div>
+                <div class="form-floating mb-3">
+                  <input name="tipe" class="form-control" value="<?php echo $row['tipe'] ?>" type="text" />
+                  <label for="tipe" class="form-label">Tipe</label>
+                </div>
 
-          <div class="form-floating mb-3">
-            <input name="merek" class="form-control" value="<?php echo $row['merek'] ?>" type="text" />
-            <label for="merek" class="form-label">Merek</label>
-          </div>
+                <div class="form-floating mb-3">
+                  <input name="merek" class="form-control" value="<?php echo $row['merek'] ?>" type="text" />
+                  <label for="merek" class="form-label">Merek</label>
+                </div>
 
-          <div class="form-floating mb-3">
-            <input name="harga" class="form-control" value="<?php echo $row['harga'] ?>" type="text" />
-            <label for="harga" class="form-label">Harga</label>
-          </div>
+                <div class="form-floating mb-3">
+                  <input name="harga" class="form-control" value="<?php echo $row['harga'] ?>" type="text" />
+                  <label for="harga" class="form-label">Harga</label>
+                </div>
 
-          <div class="form-floating mb-3">
-            <input name="layar" class="form-control" value="<?php echo $row['layar'] ?>" type="text" />
-            <label for="layar" class="form-label">Layar</label>
+                <div class="form-floating mb-3">
+                  <input name="layar" class="form-control" value="<?php echo $row['layar'] ?>" type="text" />
+                  <label for="layar" class="form-label">Layar</label>
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-floating mb-3">
+                  <input name="ram" class="form-control" value="<?php echo $row['ram'] ?>" type="text" />
+                  <label for="ram" class="form-label">RAM</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                  <input name="prosecor" class="form-control" value="<?php echo $row['prosecor'] ?>" type="text" />
+                  <label for="prosecor" class="form-label">Prosesor</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                  <input name="memory" class="form-control" value="<?php echo $row['memory'] ?>" type="text" />
+                  <label for="memory" class="form-label">Memory</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                  <input name="tanggal_rilis" class="form-control" value="<?php echo $row['tanggal_rilis'] ?>" type="date" />
+                  <label for="tanggal_rilis" class="form-label">Tanggal Rilis</label>
+                </div>
+              </div>
+            </div>
+            <div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
           </div>
         </div>
-        <div class="col">
-          <div class="form-floating mb-3">
-            <input name="ram" class="form-control" value="<?php echo $row['ram'] ?>" type="text" />
-            <label for="ram" class="form-label">RAM</label>
-          </div>
 
-          <div class="form-floating mb-3">
-            <input name="prosecor" class="form-control" value="<?php echo $row['prosecor'] ?>" type="text" />
-            <label for="prosecor" class="form-label">Prosesor</label>
-          </div>
-
-          <div class="form-floating mb-3">
-            <input name="memory" class="form-control" value="<?php echo $row['memory'] ?>" type="text" />
-            <label for="memory" class="form-label">Memory</label>
-          </div>
-
-          <div class="form-floating mb-3">
-            <input name="tanggal_rilis" class="form-control" value="<?php echo $row['tanggal_rilis'] ?>" type="date" />
-            <label for="tanggal_rilis" class="form-label">Tanggal Rilis</label>
-          </div>
-        </div>
-        <div>
-          <button action="submit" class="btn btn-primary">Submit</button>
-        </div>
       </form>
     <?php endwhile; ?>
   </div>
--- viewSuplier.php
+++ viewSuplier.php
@@ -18,30 +18,30 @@ $result = mysqli_query($koneksi, $query);
       <a href="tambahSuplier.php" class="btn btn-outline-primary" role="button">Tambah Data</a>
     </div>
     <table class="table table-striped responsive nowrap" style="width:100%" id="myTable">
-  <thead>
-    <tr>
-      <th>Id Suplier</th>
-      <th>Nama Suplier</th>
-      <th>No.Telp</th>
-      <th>Alamat</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-      <tr>
-        <td><?php echo $row['id_suplier'] ?> </td>
-        <td><?php echo $row['nama_suplier'] ?> </td>
-        <td><?php echo $row['no_telp'] ?> </td>
-        <td><?php echo $row['alamat'] ?> </td>
-        <td>
-          <a href="viewEditSuplier.php?id_suplier=<?php echo $row['id_suplier']; ?>" class="btn btn-warning btn-sm" role="button">Edit</a>
-          <a href="actionDeleteSuplier.php?id_suplier=<?php echo $row['id_suplier']; ?>" onclick="return confirm('Data ini akan dihapus?')" role="button" class="btn btn-danger btn-sm">Delete</a>
-        </td>
-      </tr>
-    <?php } ?>
-  </tbody>
-</table>
+      <thead>
+        <tr>
+          <th>Id Suplier</th>
+          <th>Nama Suplier</th>
+          <th>No.Telp</th>
+          <th>Alamat</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+          <tr>
+            <td><?php echo $row['id_suplier'] ?> </td>
+            <td><?php echo $row['nama_suplier'] ?> </td>
+            <td><?php echo $row['no_telp'] ?> </td>
+            <td><?php echo $row['alamat'] ?> </td>
+            <td>
+              <a href="viewEditSuplier.php?id_suplier=<?php echo $row['id_suplier']; ?>" class="btn btn-warning btn-sm" role="button">Edit</a>
+              <a href="actionDeleteSuplier.php?id_suplier=<?php echo $row['id_suplier']; ?>" onclick="return confirm('Data ini akan dihapus?')" role="button" class="btn btn-danger btn-sm">Delete</a>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
   </div>
   <?php include 'scripts.php'; ?>
 </body>
